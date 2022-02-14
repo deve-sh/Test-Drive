2cdbb8e6ba2d62498d104fba32125f938cc31d4f
Devesh Kumar <devesh2027@gmail.com>
2022-02-14 22:12:28 +0530

    Added log outputs and stats in tests cases

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // A simple test runner.
-// Run it like 'testdrive'
+// Run it like 'testdrive <directory/filename>'
+
+const testDirectory = process.argv[2];
+
+if (!testDirectory)
+	return console.error("Please provide a directory/file name to run tests on.");
+
+const fs = require("fs");
+const path = require("path");
+const { performance } = require("perf_hooks");
+
+const directoryName = path.resolve(process.cwd(), testDirectory);
+const directoryStat = fs.statSync(directoryName);
 
 // Initialize counts
 global.testCount = 0;
@@ -10,4 +22,34 @@ global.erroredCase = false;
 global.callStackForTests = [];
 global.currentBlockTestCaseCount = 0;
 
-require("./TestDrive");
+require("./TestDrive"); // Initialize functions to run tests.
+
+const startTime = performance.now();
+if (directoryStat.isDirectory()) {
+	// Recursively go to each file inside the directory, check if it has '.test' in its name
+	const getAllFiles = require("./getAllFiles");
+	const testFiles = getAllFiles(directoryName).filter((fileName) =>
+		fileName.includes(".test")
+	);
+	for (let i = 0; i < testFiles; i++) {
+		console.log(`Running Tests from ${testFiles[i]}`);
+		require(testFiles[i]);
+	}
+} else {
+	// It's a normal file. Just require it and run the test cases.
+	let fileName = directoryName;
+	console.log(`Running Tests from ${fileName}`);
+	require(fileName);
+}
+
+// Print out Stats after run
+if (!global.erroredCase) {
+	const finishTime = performance.now();
+	console.log(
+		"Finished Running Tests in ",
+		parseInt(finishTime - startTime) + "ms"
+	);
+	console.log("Suites Run: ", global.testSuiteCount);
+	console.log("Test Cases Run: ", global.testCount);
+	process.exit(0);
+} else process.exit(1); // Errored Exit
